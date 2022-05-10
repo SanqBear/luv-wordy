ed6dce0c2ed8b9ce274e71fd2e5604ab2e2074db
sanqbear <sanqbear@outlook.com>
2022-05-10 10:33:21 +0900

    [#1] Add swashbuckle swagger ui

--- server/LuvWordy.Server.Web/Program.cs
+++ server/LuvWordy.Server.Web/Program.cs
@@ -4,11 +4,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    // Configure the HTTP request pipeline.
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
