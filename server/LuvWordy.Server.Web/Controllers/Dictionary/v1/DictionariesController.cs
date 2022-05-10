5787050f5a97cad6274429d5d001b06643d4ca0a
sanqbear <sanqbear@outlook.com>
2022-05-10 10:26:46 +0900

    [#1] Add api controllers schema

---
+++ server/LuvWordy.Server.Web/Controllers/Dictionary/v1/DictionariesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuvWordy.Server.Web.Controllers.Dictionary
+{
+    [ApiController]
+    [Route("api/dictionaries")]
+    public class DictionariesController : ControllerBase
+    {
+        private readonly ILogger<DictionariesController> _logger;
+
+        public DictionariesController(ILogger<DictionariesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("hello")]
+        public async Task<IActionResult> HelloWorld()
+        {
+            return Ok("world");
+        }
+
+    }
+}
---
+++ server/LuvWordy.Server.Web/Controllers/Quiz/v1/QuizController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuvWordy.Server.Web.Controllers.Quiz
+{
+    [ApiController]
+    [Route("api/quiz")]
+    public class QuizController : ControllerBase
+    {
+        private readonly ILogger<QuizController> _logger;
+
+        public QuizController(ILogger<QuizController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("hello")]
+        public async Task<IActionResult> HelloWorld()
+        {
+            return Ok("world");
+        }
+
+    }
+}
