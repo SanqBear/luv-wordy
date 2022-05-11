e8214f1edafcf97298cf09939a627ac7d04606f0
sanqbear <sanqbear@outlook.com>
2022-05-11 12:05:10 +0900

    [#2,#3] Add backend razor pages for showing clients

---
+++ server/LuvWordy.Server.Web/Pages/Dictionary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LuvWordy.Server.Web.Pages
+{
+    public class DictionaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
