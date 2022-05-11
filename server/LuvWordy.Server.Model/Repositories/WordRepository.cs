76701cc4ca9fb9f197001d8ed646fd3b435d3793
sanqbear <sanqbear@outlook.com>
2022-05-11 10:29:33 +0900

    [#1] Add repo context for search feature

--- server/LuvWordy.Server.Model/Repositories/WordRepository.cs
+++ server/LuvWordy.Server.Model/Repositories/WordRepository.cs
@@ -1,4 +1,6 @@
-﻿using LuvWordy.Server.Model.Models;
+﻿using LuvWordy.Server.Model.Enums;
+using LuvWordy.Server.Model.Models;
+using LuvWordy.Server.Model.Utils;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -56,7 +58,7 @@ namespace LuvWordy.Server.Model.Repositories
             {
                 if (ds?.Tables?.Count > 0 && ds.Tables[0].Rows?.Count > 0)
                 {
-                    foreach(DataRow row in ds.Tables[0].Rows)
+                    foreach (DataRow row in ds.Tables[0].Rows)
                     {
                         wordItems.Add(new WordItem(row));
                     }
@@ -64,6 +66,59 @@ namespace LuvWordy.Server.Model.Repositories
             }
 
             return wordItems;
+        }
+
+        public (int totalCount, List<WordItemSummary> items) SearchWordItems(int size, int offset, string keyword
+            , LexicalUnitType lexicalUnit = LexicalUnitType.NotInterested
+            , PartOfSpeechType partOfSpeech = PartOfSpeechType.NotInterested
+            , VocabularyLevelType vocabularyLevel = VocabularyLevelType.NotInterested)
+        {
+            int totalCount = -1;
+            List<WordItemSummary> wordItems = new List<WordItemSummary>();
+
+            var sizeParam = new SqlParameter("Size", DbType.Int32);
+            var offsetParam = new SqlParameter("Offset", DbType.Int32);
+
+            sizeParam.Value = size;
+            offsetParam.Value = offset;
+
+            List<SqlParameter> parameters = new List<SqlParameter>()
+            {
+                sizeParam,
+                offsetParam,
+                
+            };
+            parameters.AddRange(BuildSearchParams(keyword, lexicalUnit, partOfSpeech, vocabularyLevel));
+
+            using (DataSet ds = ExecuteDataSet("[dbo].[up_SEARCH_tb_Word]", parameters.ToArray()))
+            {
+                if (ds?.Tables?.Count > 1)
+                {
+                    totalCount = int.TryParse(ds.Tables[0].Rows[0]["ItemCount"]?.ToString(), out int tc) ? tc : -1;
+
+                    foreach (DataRow row in ds.Tables[1].Rows)
+                    {
+                        wordItems.Add(new WordItemSummary(row));
+                    }
+                }
+            }
+
+            return (totalCount, wordItems);
+        }
+
+        private SqlParameter[] BuildSearchParams(string keyword, LexicalUnitType lexicalUnit, PartOfSpeechType partOfSpeech, VocabularyLevelType vocabularyLevel)
+        {
+            var keywordParam = new SqlParameter("Keyword", DbType.String);
+            var lexicalUnitParam = new SqlParameter("LexicalUnit", DbType.String);
+            var partofSpeechParam = new SqlParameter("PartOfSpeech", DbType.String);
+            var vocabularyLevelParam = new SqlParameter("VocabularyLevel", DbType.String);
+
+            keywordParam.Value = string.IsNullOrWhiteSpace(keyword) ? DBNull.Value : keyword;
+            lexicalUnitParam.Value = lexicalUnit == LexicalUnitType.NotInterested ? DBNull.Value : LexicalUnit.ToString(lexicalUnit);
+            partofSpeechParam.Value = partOfSpeech == PartOfSpeechType.NotInterested ? DBNull.Value : PartOfSpeech.ToString(partOfSpeech);
+            vocabularyLevelParam.Value = vocabularyLevel == VocabularyLevelType.NotInterested ? DBNull.Value : VocabularyLevel.ToString(vocabularyLevel);
+
+            return new SqlParameter[] { keywordParam, lexicalUnitParam, partofSpeechParam, vocabularyLevelParam };
         }
 
         private DataSet ExecuteDataSet(string query, SqlParameter[]? parameters = null, CommandType commandType = CommandType.StoredProcedure)
