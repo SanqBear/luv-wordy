4ff56f3ee327001f51ed10b837581226f8319d12
sanqbear <sanqbear@outlook.com>
2022-05-11 11:47:48 +0900

    [#2,#3] Add vue base app

---
+++ client/packages/applications/luvwordy-dictionary/src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>{DICTIONARY APP 입니다}</div>
+</template>
---
+++ client/packages/applications/luvwordy-quiz/src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>{QUIZ APP 입니다}</div>
+</template>
